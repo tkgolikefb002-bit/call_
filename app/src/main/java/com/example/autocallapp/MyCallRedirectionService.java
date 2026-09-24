67aa0a029ec44e0bc848157a5ae6896e0ca123f9
tkgolikefb002-bit <tkgolikefb002@gmail.com>
2026-09-24 22:20:09 +0700

    Update MyCallRedirectionService.java

--- app/src/main/java/com/example/autocallapp/MyCallRedirectionService.java
+++ app/src/main/java/com/example/autocallapp/MyCallRedirectionService.java
@@ -10,19 +10,19 @@ import androidx.annotation.RequiresApi;
 @RequiresApi(api = Build.VERSION_CODES.Q)
 public class MyCallRedirectionService extends CallRedirectionService {
 
-    // Số điện thoại mặc định bạn muốn chuyển hướng tới
-    private static final String TARGET_PHONE_NUMBER = "09012345670";
+    // SỐ ẢO MÀ BẠN MUỐN CHUYỂN HƯỚNG TỚI THAY CHO SỐ GỐC
+    private static final String VIRTUAL_PHONE_NUMBER = "09012345670";
 
     @Override
     public void onPlaceCall(@NonNull Uri handle, @NonNull PhoneAccountHandle initialPhoneAccount, boolean allowFullScreenIntent) {
         String currentNumber = handle.getSchemeSpecificPart();
         
-        // Nếu số điện thoại đang gọi chính là số đích thì cho phép đi thẳng (tránh lặp vô tận)
-        if (currentNumber != null && currentNumber.equals(TARGET_PHONE_NUMBER)) {
+        // Nếu số hiện tại đã là số ảo thì cho đi thẳng để tránh lặp vô tận
+        if (currentNumber != null && currentNumber.equals(VIRTUAL_PHONE_NUMBER)) {
             placeCallUnmodified();
         } else {
-            // Ngược lại, ép chuyển hướng toàn bộ các số khác sang số mặc định
-            Uri redirectedUri = Uri.fromParts("tel", TARGET_PHONE_NUMBER, null);
+            // Bẻ lái toàn bộ các số gọi khác sang SỐ ẢO
+            Uri redirectedUri = Uri.fromParts("tel", VIRTUAL_PHONE_NUMBER, null);
             redirectCall(redirectedUri, initialPhoneAccount, false);
         }
     }
